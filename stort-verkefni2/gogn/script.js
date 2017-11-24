c0166b4ed2d2440097f7a8cef3a8c59ddc5d0471
ema22 <ema22@hi.is>
2017-11-24 19:02:31 +0000

    uppfaert script fyrir index

--- stort-verkefni2/gogn/script.js
+++ stort-verkefni2/gogn/script.js
@@ -1,29 +1,44 @@
 'use strict';
-var picture1 = document.createElement("IMG");
-var picture2 = document.createElement("IMG");
-var picture3 = document.createElement("IMG");
-var picture4 = document.createElement("IMG");
+var cat1 = document.createElement('text1');
+var picture1 = document.createElement('img');
+var picture2 = document.createElement('img');
+var picture3 = document.createElement('img');
+var picture4 = document.createElement('img');
 var video1 = document.getElementById('1');
 var video2 = document.getElementById('2');
 var video3 = document.getElementById('3');
 var video4 = document.getElementById('4');
 var source = document.createElement('source');
 
+
 $.getJSON( "videos.json", function( data )  {
+    categories(data.categories);
     addVideo(data.videos);
   });
 
-function playVideo(data){
-  video1.setAttribute("src", data[1].video);
-  video1.appendChild(source);
-  video1.play();
-}
+  function categories(data){
+    var div1 = document.createElement('text1');
+    cat1.setAttribute("src", data[0].title);
+    div1.appendChild(cat1);
+    div1.querySelector('text1');
+    div1.textContent = data[0].title;
+
+
+    document.body.appendChild(div1);
+  }
 
 function addVideo(data){
+  var div2 = document.createElement('row1');
   picture1.setAttribute("src", data[0].poster);
-  picture1.setAttribute("width", "20");
-  picture1.setAttribute("height", "20");
-  picture1.setAttribute("alt", "test");
-  document.body.appendChild(picture1);
-  playVideo(data);
+  div2.appendChild(picture1);
+  picture2.setAttribute("src", data[1].poster);
+  div2.appendChild(picture2);
+
+  document.body.appendChild(div2);
+}
+
+
+
+class forsida{
+
 }
