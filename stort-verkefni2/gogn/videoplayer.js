43d4279183bba6a5db81b8284ded4a2a89a47dec
alli959 <alg35@hi.is>
2017-11-27 18:09:12 +0000

    update á videoplayer

--- stort-verkefni2/gogn/videoplayer.js
+++ stort-verkefni2/gogn/videoplayer.js
@@ -4,10 +4,16 @@ var video = document.getElementById('videoplay');
 
 var source = document.createElement('source');
 
+var back = document.createElement('p');
+
+var playing = false;
+
+
 
 
 $.getJSON( "videos.json", function( data )  {
     playVideo(data.videos);
+    goback();
   });
 
 function playVideo(data){
@@ -40,6 +46,8 @@ function fullscreen(){
 
 function mute(){
   video.muted = true;
+  document.getElementById("mute").style.display = 'none';
+  document.getElementById("unmute").style.display = 'inline-block';
 }
 
 function next(){
@@ -48,12 +56,44 @@ function next(){
 
 function pause(){
   video.pause();
+  document.getElementById("pause").style.display = 'none';
+  document.getElementById("play").style.display = 'inline-block';
 }
 
 function play(){
   video.play();
+  document.getElementById("play").style.display = 'none';
+  document.getElementById("pause").style.display = 'inline-block';
 }
 
 function unmute(){
   video.muted = false;
+  document.getElementById("unmute").style.display = 'none';
+  document.getElementById("mute").style.display = 'inline-block';
+}
+
+video.onclick = function(){
+  if(playing){
+    pause();
+    playing = false;
+  }
+  else{
+    play();
+    playing = true;
+  }
+}
+
+function goback(){
+  var theText = document.createElement('p');
+  back.setAttribute("Til baka");
+  theText.appendChild(back);
+  theText.querySelector('p');
+  theText.textContent = "Til baka";
+  document.body.appendChild(theText);
+  /*var div1 = document.createElement('text1');
+  cat1.setAttribute("src", data[0].title);
+  div1.appendChild(cat1);
+  div1.querySelector('text1');
+  div1.textContent = data[0].title;
+  document.body.appendChild(div1);*/
 }
